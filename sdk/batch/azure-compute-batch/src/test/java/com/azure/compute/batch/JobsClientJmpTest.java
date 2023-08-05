cd6b79738e8048078e1392f67e710659ecd54ee7
Srikanta Nagaraja <srnagar@microsoft.com>
2023-08-05 13:28:03 -0700

    Azure Batch codegen

---
+++ sdk/batch/azure-compute-batch/src/test/java/com/azure/compute/batch/JobsClientJmpTest.java
@@ -0,0 +1,46 @@
+package com.azure.compute.batch;
+
+import com.azure.compute.batch.models.BatchJob;
+import com.azure.compute.batch.models.BatchJobUpdateParameters;
+import com.azure.compute.batch.models.JobConstraints;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.util.BinaryData;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+public class JobsClientJmpTest {
+
+    @Test
+    public void testJobsClientJmpTest() {
+        JobClient jobClient = new BatchServiceClientBuilder()
+            .endpoint("https://srnagarbatch.westus.batch.azure.com")
+            .credential(new DefaultAzureCredentialBuilder().build())
+            .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
+            .buildJobClient();
+
+        System.out.println("First GET");
+        BatchJob testpatch = jobClient.get("testpatch");
+        System.out.println(BinaryData.fromObject(testpatch));
+
+        System.out.println("PATCH with max retry count updated");
+        BatchJobUpdateParameters batchUpdateParams = new BatchJobUpdateParameters(testpatch.getPoolInfo())
+            .setConstraints(new JobConstraints().setMaxTaskRetryCount(10));
+        jobClient.patchJmp("testpatch", batchUpdateParams);
+
+        System.out.println("GET after max retry count updated");
+        testpatch = jobClient.get("testpatch");
+        System.out.println(BinaryData.fromObject(testpatch));
+
+        System.out.println("PATCH with max wall clock time updated");
+        batchUpdateParams = new BatchJobUpdateParameters(testpatch.getPoolInfo())
+            .setConstraints(new JobConstraints().setMaxWallClockTime(Duration.ofDays(100)));
+        jobClient.patchJmp("testpatch", batchUpdateParams);
+
+        System.out.println("GET after max wall clock time updated");
+        testpatch = jobClient.get("testpatch");
+        System.out.println(BinaryData.fromObject(testpatch));
+    }
+}
