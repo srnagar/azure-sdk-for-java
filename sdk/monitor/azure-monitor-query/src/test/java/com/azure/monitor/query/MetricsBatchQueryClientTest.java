f943525cc6b6e1519c11cd3833403aac453e51d7
Srikanta Nagaraja <srnagar@microsoft.com>
2023-08-11 13:07:05 -0700

    Azure Monitor: add more tests for batch query

--- sdk/monitor/azure-monitor-query/src/test/java/com/azure/monitor/query/MetricsBatchQueryClientTest.java
+++ sdk/monitor/azure-monitor-query/src/test/java/com/azure/monitor/query/MetricsBatchQueryClientTest.java
@@ -3,6 +3,7 @@
 
 package com.azure.monitor.query;
 
+import com.azure.core.exception.HttpResponseException;
 import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.http.policy.HttpLogOptions;
 import com.azure.core.test.annotation.RecordWithoutRequestBody;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Unit tests for {@link MetricsBatchQueryClient}.
@@ -25,12 +27,29 @@ public class MetricsBatchQueryClientTest extends MetricsBatchQueryTestBase {
         MetricsBatchQueryClient metricsBatchQueryClient = clientBuilder
             .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
             .buildClient();
-        String resourceId = Configuration.getGlobalConfiguration().get("AZURE_MONITOR_METRICS_RESOURCE_URI", FAKE_RESOURCE_ID);
+        String resourceId = Configuration.getGlobalConfiguration().get("AZURE_MONITOR_METRICS_RESOURCE_URI_1", FAKE_RESOURCE_ID);
         resourceId = resourceId.substring(resourceId.indexOf("/subscriptions"));
 
         MetricsBatchResult metricsQueryResults = metricsBatchQueryClient.queryBatch(
             Arrays.asList(resourceId),
             Arrays.asList("Successful Requests"), " Microsoft.Eventhub/Namespaces");
         assertEquals(1, metricsQueryResults.getMetricsQueryResults().size());
+    }
+
+    @Test
+    @RecordWithoutRequestBody
+    public void testMetricsBatchQueryDifferentResourceTypes() {
+        MetricsBatchQueryClient metricsBatchQueryClient = clientBuilder
+            .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
+            .buildClient();
+        String resourceId1 = Configuration.getGlobalConfiguration().get("AZURE_MONITOR_METRICS_RESOURCE_URI_1", FAKE_RESOURCE_ID);
+        String resourceId2 = Configuration.getGlobalConfiguration().get("AZURE_MONITOR_METRICS_RESOURCE_URI_2", FAKE_RESOURCE_ID);
+        String updatedResource1 = resourceId1.substring(resourceId1.indexOf("/subscriptions"));
+        String updatedResource2 = resourceId2.substring(resourceId2.indexOf("/subscriptions"));
+
+        assertThrows(HttpResponseException.class, () -> metricsBatchQueryClient.queryBatch(
+            Arrays.asList(updatedResource1, updatedResource2),
+            Arrays.asList("Successful Requests"), " Microsoft.Eventhub/Namespaces"));
+
     }
 }
