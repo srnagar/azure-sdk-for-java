2bf3d883bc2688b3e3c1e2492bd49ed6e05bc6dd
Srikanta <srnagar@microsoft.com>
2026-02-10 00:06:36 -0800

    update search checkstyle

--- sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/indexes/models/SearchIndexerKnowledgeStoreProjectionSelector.java
+++ sdk/search/azure-search-documents/src/main/java/com/azure/search/documents/indexes/models/SearchIndexerKnowledgeStoreProjectionSelector.java
@@ -7,9 +7,7 @@ package com.azure.search.documents.indexes.models;
 
 import com.azure.core.annotation.Fluent;
 import com.azure.core.annotation.Generated;
-import com.azure.json.JsonReader;
 import com.azure.json.JsonSerializable;
-import com.azure.json.JsonToken;
 import com.azure.json.JsonWriter;
 import java.io.IOException;
 import java.util.List;
@@ -181,39 +179,5 @@ public class SearchIndexerKnowledgeStoreProjectionSelector
         jsonWriter.writeStringField("sourceContext", this.sourceContext);
         jsonWriter.writeArrayField("inputs", this.inputs, (writer, element) -> writer.writeJson(element));
         return jsonWriter.writeEndObject();
-    }
-
-    /**
-     * Reads an instance of SearchIndexerKnowledgeStoreProjectionSelector from the JsonReader.
-     *
-     * @param jsonReader The JsonReader being read.
-     * @return An instance of SearchIndexerKnowledgeStoreProjectionSelector if the JsonReader was pointing to an
-     * instance of it, or null if it was pointing to JSON null.
-     * @throws IOException If an error occurs while reading the SearchIndexerKnowledgeStoreProjectionSelector.
-     */
-    public static SearchIndexerKnowledgeStoreProjectionSelector fromJson(JsonReader jsonReader) throws IOException {
-        return jsonReader.readObject(reader -> {
-            SearchIndexerKnowledgeStoreProjectionSelector deserializedSelector
-                = new SearchIndexerKnowledgeStoreProjectionSelector();
-            while (reader.nextToken() != JsonToken.END_OBJECT) {
-                String fieldName = reader.getFieldName();
-                reader.nextToken();
-
-                if ("referenceKeyName".equals(fieldName)) {
-                    deserializedSelector.referenceKeyName = reader.getString();
-                } else if ("generatedKeyName".equals(fieldName)) {
-                    deserializedSelector.generatedKeyName = reader.getString();
-                } else if ("source".equals(fieldName)) {
-                    deserializedSelector.source = reader.getString();
-                } else if ("sourceContext".equals(fieldName)) {
-                    deserializedSelector.sourceContext = reader.getString();
-                } else if ("inputs".equals(fieldName)) {
-                    deserializedSelector.inputs = reader.readArray(InputFieldMappingEntry::fromJson);
-                } else {
-                    reader.skipChildren();
-                }
-            }
-            return deserializedSelector;
-        });
     }
 }
